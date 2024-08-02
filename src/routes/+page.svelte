ade90299d32bd86ddda0086f793a74dbab751d11
Jonas Bulik <jonas@bulik.dev>
2024-08-02 12:33:26 +0200

    Move sources

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,6 @@
 <script lang="ts">
-  const sources = ["Mail", "Contacts", "Calendar", "Notes"] as const;
-  type Source = {
-    fields: string[];
-    content: Record<string, string>;
-  };
-  const all_sources: { [key in (typeof sources)[number]]: Source } = {
-    Mail: {
-      fields: ["From", "To", "Subject", "Body"],
-      content: {
-        From: "Rebecca",
-        To: "Jonas",
-        Subject: "Hallo",
-        Body: "Gute Nachricht.",
-      },
-    },
-    Contacts: {
-      fields: ["Name", "Email", "Phone", "Address"],
-      content: {
-        Name: "Rebecca",
-        Email: "Rebecca@me.com",
-        Phone: "123456789",
-        Address: "Somewhere",
-      },
-    },
-    Calendar: {
-      fields: ["Date", "Time", "Title", "Description"],
-      content: {
-        Date: "2022-01-01",
-        Time: "12:00",
-        Title: "Rebecca treffen",
-        Description: "Einen schönen Abend.",
-      },
-    },
-    Notes: {
-      fields: ["Title", "Body"],
-      content: {
-        Title: "Meine Ideen",
-        Body: "Gute Ideen.",
-      },
-    },
-  };
+  import { sources, all_sources } from "$lib/source";
+
   let selected = sources[0];
   $: current_source = all_sources[selected];
   let selected_field: string;
