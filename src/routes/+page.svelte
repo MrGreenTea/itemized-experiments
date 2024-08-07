4fb685f8c6260e447224d021b8d6e6c6e3275c37
Jonas Bulik <jonas@bulik.dev>
2024-08-07 10:45:06 +0200

    Move experiments into separate routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,16 @@
-<script lang="ts">
-  import ListOfComponent from "$lib/components/list_of_component.svelte";
-  import MappedCard, { schema } from "$lib/components/mapped_card.svelte";
-  import MappingEditor from "$lib/components/mapping_editor.svelte";
-
-  const input_fields = ["id", "name", "age", "description"] as const;
-  const output_fields = Object.keys(schema.properties);
-
-  const items: { [key in (typeof input_fields)[number]]: any }[] = [
-    {
-      id: 1,
-      name: "Jonas",
-      description: "pretty awesome guy",
-      age: 23,
-    },
-    {
-      id: 2,
-      name: "Cool Guy",
-      description: "very cool guy",
-      age: 42,
-    },
-    {
-      id: 3,
-      name: "Cute Baby",
-      description: "a very cute baby",
-      age: 2,
-    },
-  ];
-
-  let mapping = {
-    title: "name",
-    description: "description",
-    subtitle: "age",
-    contact: "id",
-  };
+<script>
+  const allRoutes = import.meta.glob("./**/+page.svelte");
+  const routes = Object.keys(allRoutes).map((r) =>
+    r.replace("./", "").replace("+page.svelte", ""),
+  );
 </script>
 
-<div class="mx-auto max-w-4xl">
-  <MappingEditor bind:mapping {input_fields} {output_fields} />
-  <ListOfComponent component={MappedCard} {items} {mapping} />
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+  <ul class="gap-4 divide-y">
+    {#each routes as route}
+      <li class="py-2">
+        <a class="text-xl text-blue-600" href={route}>{route}</a>
+      </li>
+    {/each}
+  </ul>
 </div>
---
+++ src/routes/list/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import ListOfComponent from "$lib/components/list_of_component.svelte";
+  import MappedCard, { schema } from "$lib/components/mapped_card.svelte";
+  import MappingEditor from "$lib/components/mapping_editor.svelte";
+
+  const input_fields = ["id", "name", "age", "description"] as const;
+  const output_fields = Object.keys(schema.properties);
+
+  const items: { [key in (typeof input_fields)[number]]: any }[] = [
+    {
+      id: 1,
+      name: "Jonas",
+      description: "pretty awesome guy",
+      age: 23,
+    },
+    {
+      id: 2,
+      name: "Cool Guy",
+      description: "very cool guy",
+      age: 42,
+    },
+    {
+      id: 3,
+      name: "Cute Baby",
+      description: "a very cute baby",
+      age: 2,
+    },
+  ];
+
+  let mapping = {
+    title: "name",
+    description: "description",
+    subtitle: "age",
+    contact: "id",
+  };
+</script>
+
+<div class="mx-auto max-w-4xl">
+  <MappingEditor bind:mapping {input_fields} {output_fields} />
+  <ListOfComponent component={MappedCard} {items} {mapping} />
+</div>
